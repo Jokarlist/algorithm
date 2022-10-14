519ac9be090602f04e03654f41a8f45c79fae9a3
heungkuen <purebloodgk@gmail.com>
2022-10-14 21:17:04 +0800

    完善链表部分

--- index_3.js
+++ index_3.js
@@ -5,13 +5,13 @@ class Node {
   }
 }
 
-/* class Node {
+class DoubleNode {
   constructor(value) {
     this.value = value;
     this.next = null;
     this.last = null;
   }
-} */
+}
 
 const node1 = new Node(2);
 node1.next = new Node(3);
@@ -24,6 +24,14 @@ node2.next.next = new Node(5);
 node2.next.next.next = new Node(7);
 node2.next.next.next.next = new Node(8);
 
+const node3 = new DoubleNode(1);
+node3.next = new DoubleNode(2);
+node3.next.last = node3;
+node3.next.next = new DoubleNode(3);
+node3.next.next.last = node3.next;
+node3.next.next.next = new DoubleNode(4);
+node3.next.next.next.last = node3.next.next;
+
 /**
  * 反转单向和双向链表
  *
@@ -31,7 +39,45 @@ node2.next.next.next.next = new Node(8);
  * 要求：如果链表长度为 N，时间复杂度要求为 O(N)，额外空间复杂度要求为 O(1)
  */
 
-function reveseList() {}
+function reverseList(head) {
+  let pre = null;
+  let next = null;
+  while (head) {
+    // 保存下一结点
+    next = head.next;
+    // 将当前结点的指向改变成指向上一结点
+    head.next = pre;
+    // pre 右移
+    pre = head;
+    // head 右移
+    head = next;
+  }
+
+  // pre 最后指向链表反转后的新头结点
+  return pre;
+}
+
+function reverseList2(head) {
+  let pre = null;
+  let next = null;
+  while (head) {
+    next = head.next;
+    // 当前结点的 next 改成指向上一个结点
+    head.next = pre;
+    // 当前结点的 last 改成指向下一个结点
+    head.last = next;
+    // pre 右移
+    pre = head;
+    // head 右移
+    head = next;
+  }
+
+  // pre 最后指向链表反转后的新头结点
+  return pre;
+}
+
+// console.log(reverseList(node1));
+// console.log(reverseList2(node3));
 
 /**
  * 打印两个有序链表的公共部分
@@ -191,15 +237,15 @@ function isPalindrome3(head) {
   return res;
 }
 
-const head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(2);
-head.next.next.next.next = new Node(1);
+const node4 = new Node(1);
+node4.next = new Node(2);
+node4.next.next = new Node(3);
+node4.next.next.next = new Node(2);
+node4.next.next.next.next = new Node(1);
 
-// console.log(isPalindrome(head));
-// console.log(isPalindrome2(head));
-// console.log(isPalindrome3(head));
+// console.log(isPalindrome(node4));
+// console.log(isPalindrome2(node4));
+// console.log(isPalindrome3(node4));
 
 /**
  * 将单向链表按某值划分成左边小、中间相等、右边大的形式
@@ -328,16 +374,16 @@ function listPartition2(head, pivot) {
   return !!sH ? sH : !!eH ? eH : bH;
 }
 
-const head1 = new Node(7);
-head1.next = new Node(9);
-head1.next.next = new Node(1);
-head1.next.next.next = new Node(8);
-head1.next.next.next.next = new Node(5);
-head1.next.next.next.next.next = new Node(2);
-head1.next.next.next.next.next.next = new Node(5);
+const node5 = new Node(7);
+node5.next = new Node(9);
+node5.next.next = new Node(1);
+node5.next.next.next = new Node(8);
+node5.next.next.next.next = new Node(5);
+node5.next.next.next.next.next = new Node(2);
+node5.next.next.next.next.next.next = new Node(5);
 
-// console.log(JSON.stringify(listPartition(head1, 4)));
-// console.log(JSON.stringify(listPartition2(head1, 5)));
+// console.log(JSON.stringify(listPartition(node5, 4)));
+// console.log(JSON.stringify(listPartition2(node5, 5)));
 
 /**
  * 复制含有随机指针节点的链表
@@ -417,19 +463,19 @@ function copyListWithRandom2(head) {
   return res;
 }
 
-const head2 = new randNode(1);
-head2.next = new randNode(2);
-head2.next.next = new randNode(3);
-head2.next.next.next = new randNode(4);
-head2.next.next.next.next = new randNode(5);
-head2.next.next.next.next.next = new randNode(6);
+const node6 = new randNode(1);
+node6.next = new randNode(2);
+node6.next.next = new randNode(3);
+node6.next.next.next = new randNode(4);
+node6.next.next.next.next = new randNode(5);
+node6.next.next.next.next.next = new randNode(6);
 
-head2.rand = head2.next.next.next.next.next; // 1 -> 6
-head2.next.rand = head2.next.next.next.next.next; // 2 -> 6
-head2.next.next.rand = head2.next.next.next.next; // 3 -> 5
-head2.next.next.next.rand = head2.next.next; // 4 -> 3
-head2.next.next.next.next.rand = null; // 5 -> null
-head2.next.next.next.next.next.rand = head2.next.next.next; // 6 -> 4
+node6.rand = node6.next.next.next.next.next; // 1 -> 6
+node6.next.rand = node6.next.next.next.next.next; // 2 -> 6
+node6.next.next.rand = node6.next.next.next.next; // 3 -> 5
+node6.next.next.next.rand = node6.next.next; // 4 -> 3
+node6.next.next.next.next.rand = null; // 5 -> null
+node6.next.next.next.next.next.rand = node6.next.next.next; // 6 -> 4
 
 function printRandLinkedList(head) {
   let cur = head;
@@ -446,4 +492,189 @@ function printRandLinkedList(head) {
 }
 
 // printRandLinkedList(copyListWithRandom(head2));
-printRandLinkedList(copyListWithRandom2(head2));
+// printRandLinkedList(copyListWithRandom2(head2));
+
+/**
+ * 两个单链表相交的一系列问题
+ *
+ * 题目：给定两个可能有环也可能无环的单链表，头节点 head1 和 head2。请实现一个函数，如果两个链表相交，
+ * 请返回相交的第一个节点。如果不相交，返回 null。
+ *
+ * 要求：如果两个链表长度之和为 N，时间复杂度请达到 O(N)，额外空间复杂度请达到 O(1)。
+ */
+
+function findFirstIntersectNode(head1, head2) {
+  if (!head1 || !head2) {
+    return null;
+  }
+
+  const loop1 = getLoopNode(head1);
+  const loop2 = getLoopNode(head2);
+  // 两个单链表都没有环的情况
+  if (!loop1 && !loop2) {
+    return noLoop(head1, head2);
+  }
+
+  // 两个单链表都有环的情况
+  if (loop1 && loop2) {
+    return bothLoop(head1, loop1, head2, loop2);
+  }
+
+  // 一个有环一个无环时，不可能相交
+  return null;
+}
+
+// 获取单链表的入环结点，无环则返回 null
+function getLoopNode(head) {
+  // 至少要三个结点才能形成环
+  if (!head || !head.next || !head.next.next) {
+    return null;
+  }
+
+  // 慢指针 n1
+  let n1 = head.next;
+  // 快指针 n2
+  let n2 = head.next.next;
+  while (n1 !== n2) {
+    // n2 走到头都没遇到 n1，则无环
+    if (!n2.next || !n2.next.next) {
+      return null;
+    }
+
+    n1 = n1.next;
+    n2 = n2.next.next;
+  }
+
+  n2 = head;
+  while (n1 !== n2) {
+    n1 = n1.next;
+    n2 = n2.next;
+  }
+
+  return n1;
+}
+
+function noLoop(head1, head2) {
+  if (!head1 || !head2) {
+    return null;
+  }
+
+  let cur1 = head1;
+  let cur2 = head2;
+  let n = 0;
+  while (cur1.next) {
+    n++;
+    cur1 = cur1.next;
+  }
+
+  while (cur2.next) {
+    n--;
+    cur2 = cur2.next;
+  }
+
+  // cur1 指向长链表的头结点
+  cur1 = n > 0 ? head1 : head2;
+  // cur2 指向短链表的头结点
+  cur2 = cur1 === head1 ? head2 : head1;
+  n = Math.abs(n);
+  while (n > 0) {
+    n--;
+    cur1 = cur1.next;
+  }
+
+  while (cur1 !== cur2) {
+    cur1 = cur1.next;
+    cur2 = cur2.next;
+  }
+
+  return cur1;
+}
+
+function bothLoop(head1, loop1, head2, loop2) {
+  let cur1 = null;
+  let cur2 = null;
+  // 两个有环单链表的入环结点为同一个，此时求第一个相交结点，和俩无环单链表的情况一致
+  if (loop1 === loop2) {
+    cur1 = head1;
+    cur2 = head2;
+    let n = 0;
+    while (cur1 !== loop1) {
+      n++;
+      cur1 = cur1.next;
+    }
+
+    while (cur2 !== loop1) {
+      n--;
+      cur2 = cur2.next;
+    }
+
+    cur1 = n > 0 ? head1 : head2;
+    cur2 = cur1 === head1 ? head2 : head1;
+    n = Math.abs(n);
+    while (n > 0) {
+      n--;
+      cur1 = cur1.next;
+    }
+
+    while (cur1 !== cur2) {
+      cur1 = cur1.next;
+      cur2 = cur2.next;
+    }
+
+    return cur1;
+  } else {
+    // 入环结点不为同一个，此时有两种情况，相交（两个初始相交结点）或不相交
+    cur1 = loop1.next;
+    // 让 loop1 沿着环转一圈，中途中遇到 loop2 则代表俩链表相交，此时返回 loop1 或 loop2 任一即可
+    // 否则不相交，返回 null;
+    while (cur1 !== loop1) {
+      if (cur1 === loop2) {
+        return loop1;
+      }
+
+      cur1 = cur1.next;
+    }
+
+    return null;
+  }
+}
+
+// 1->2->3->4->5->6->7->null
+let head1 = new Node(1);
+head1.next = new Node(2);
+head1.next.next = new Node(3);
+head1.next.next.next = new Node(4);
+head1.next.next.next.next = new Node(5);
+head1.next.next.next.next.next = new Node(6);
+head1.next.next.next.next.next.next = new Node(7);
+
+// 0->9->8->6->7->null
+let head2 = new Node(0);
+head2.next = new Node(9);
+head2.next.next = new Node(8);
+head2.next.next.next = head1.next.next.next.next.next; // 8->6
+console.log(findFirstIntersectNode(head1, head2).value);
+
+// 1->2->3->4->5->6->7->4...
+head1 = new Node(1);
+head1.next = new Node(2);
+head1.next.next = new Node(3);
+head1.next.next.next = new Node(4);
+head1.next.next.next.next = new Node(5);
+head1.next.next.next.next.next = new Node(6);
+head1.next.next.next.next.next.next = new Node(7);
+head1.next.next.next.next.next.next = head1.next.next.next; // 7->4
+
+// 0->9->8->2...
+head2 = new Node(0);
+head2.next = new Node(9);
+head2.next.next = new Node(8);
+head2.next.next.next = head1.next; // 8->2
+console.log(findFirstIntersectNode(head1, head2).value);
+
+// 0->9->8->6->4->5->6..
+head2 = new Node(0);
+head2.next = new Node(9);
+head2.next.next = new Node(8);
+head2.next.next.next = head1.next.next.next.next.next; // 8->6
+console.log(findFirstIntersectNode(head1, head2).value);
