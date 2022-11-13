3100df43ccbe6052d00fbdbab98b401cb1d85dd6
heungkuen <purebloodgk@gmail.com>
2022-11-13 18:29:07 +0800

    对冒泡、选择、插入排序做小调整

--- index_1.1.js
+++ index_1.1.js
@@ -5,7 +5,10 @@
  * @param {number[]} arr 待排序的数组
  */
 const bubbleSort = function (arr) {
-  if (!arr || arr.length < 2) return;
+  if (!arr || arr.length < 2) {
+    return arr;
+  }
+
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
@@ -24,7 +27,10 @@ const bubbleSort = function (arr) {
  * @param {number[]} arr 待排序的数组
  */
 const selectionSort = function (arr) {
-  if (!arr || arr.length < 2) return;
+  if (!arr || arr.length < 2) {
+    return arr;
+  }
+
   for (let i = 0; i < arr.length - 1; i++) {
     let minIndex = i;
     for (let j = i + 1; j < arr.length; j++) {
@@ -46,7 +52,10 @@ const selectionSort = function (arr) {
  * @param {number[]} arr 待排序的数组
  */
 const insertionSort = function (arr) {
-  if (!arr || arr.length < 2) return;
+  if (!arr || arr.length < 2) {
+    return arr;
+  }
+
   for (let i = 1; i < arr.length; i++) {
     for (let j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
       swap(arr, j, j + 1);
